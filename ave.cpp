1f6f276fe63833165c3603927e4afce501c2b150
shumon84 <cameremon84@gmail.com>
2018-01-02 23:23:48 +0900

    平均を求める関数を追加

---
+++ ave.cpp
@@ -0,0 +1,8 @@
+double average(double array[], uint32_t element_number);
+double average(double array[], uint32_t element_number){
+  uint32_t i;
+  double sum=0;
+  for(i=0;i<element_number;i++)
+    sum+=array[i];
+  return sum/element_number;
+}
