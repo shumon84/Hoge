5e03d2db350ba612f3397918c2a723680f571bce
shumon84 <cameremon84@gmail.com>
2018-01-27 15:08:41 +0900

    EncFile.hを修正

--- lobel/EncFile.h
+++ lobel/EncFile.h
@@ -3,12 +3,19 @@
 #include<stddef.h>
 #include"BinFile.h"
 
-const size_t EF_OUTOBMEMORY = 0xFFFFFFFF; // memory is not suffient
+const size_t EF_OUTOFMEMORY = 0xFFFFFFFF; // memory is not suffient
 
 class CEncodeFile : public CBinaryFile{
 public:
   CEncodeFile();
-  CEncodeFile(const void *buffer, size_t nSize);
+  CEncodeFile(const char *pszPath, const char *pszFlags);
+  CEncodeFile(const CEncodeFile &other);
+
+  virtual ~CEncodeFile();
+
+public:
+  virtual size_t Read(void *buffer, size_t nSize);
+  virtual size_t Write(const void *buffer,size_t nSize );
 
 private:
   virtual void Encode(void *bufDest, const void * bufSrc, size_t nSize);
