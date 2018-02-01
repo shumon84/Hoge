e2e3eb6edc24ae6eaf5e4d6c3757a067ec4b38d2
shumon84 <cameremon84@gmail.com>
2018-02-01 11:13:44 +0900

    main.cppを修正

--- lobel/IntArray/main.cpp
+++ lobel/IntArray/main.cpp
@@ -1,4 +1,29 @@
-#include<stdio.h>
+#include<iostream>
 #include"IntArray.h"
-int main(){
+void DispArray(const CIntArray& array)
+{
+  int i;
+
+  std::cout << "要素数 : " << array.NumOf() << std::endl;
+  std::cout << "要素 : ";
+  for(i = 0; i < array.NumOf(); i++)
+    std::cout << array[i] << ' ';
+  std::cout << std::endl << std::endl;
+}
+
+int main()
+{
+  CIntArray a1(1), a2(1), a3(10);
+  int       i;
+
+  for(i = 0; i < a3.NumOf(); i++)
+    a3[i] = i;
+
+  a1 = a2 = a3;  // 例の代入
+
+  DispArray(a1);
+  DispArray(a2);
+  DispArray(a3);
+
+  return 0;
 }
