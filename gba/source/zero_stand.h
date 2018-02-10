26206243ed01d838a98951817666605ad08fce8d
shumon84 <cameremon84@gmail.com>
2018-02-10 23:31:50 +0900

    zero_stand.hを修正

--- gba/source/zero_stand.h
+++ gba/source/zero_stand.h
@@ -1,5 +1,3 @@
 #ifndef zero_stand_H
 #define zero_stand_H
-extern const unsigned short zero_stand_pal[256];
-extern const unsigned short zero_stand[1600];
-#endif
+extern const unsigned short IMG_zero_stand[2560];#endif
