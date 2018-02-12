5eeb29479e3d49e52362cf67092f00ca64e723d0
shumon84 <cameremon84@gmail.com>
2018-02-13 00:07:27 +0900

    clip_2.hを追加

---
+++ gba/source/clip_2.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(clip_2)
+
+//======================================================================
+//
+//	clip_2, 256x128@4, 
+//	+ palette 16 entries, not compressed
+//	+ 512 tiles not compressed
+//	Total size: 32 + 16384 = 16416
+//
+//	Time-stamp: 2018-02-12, 23:41:16
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CLIP_2_H
+#define GRIT_CLIP_2_H
+
+#define clip_2TilesLen 16384
+extern const unsigned short clip_2Tiles[8192];
+
+#define clip_2PalLen 32
+extern const unsigned short clip_2Pal[16];
+
+#endif // GRIT_CLIP_2_H
+
+//}}BLOCK(clip_2)
