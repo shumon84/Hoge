d1439439149a199deaa4fd7ecd0e22ae0749f9ad
shumon84 <cameremon84@gmail.com>
2018-02-16 23:05:49 +0900

    chr.hを追加

---
+++ gba/source/chr.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(chr)
+
+//======================================================================
+//
+//	chr, 128x112@4, 
+//	+ palette 256 entries, not compressed
+//	+ 224 tiles not compressed
+//	Total size: 512 + 7168 = 7680
+//
+//	Time-stamp: 2018-02-13, 17:11:08
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.13
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_CHR_H
+#define GRIT_CHR_H
+
+#define chrTilesLen 7168
+extern const unsigned short chrTiles[3584];
+
+#define chrPalLen 512
+extern const unsigned short chrPal[256];
+
+#endif // GRIT_CHR_H
+
+//}}BLOCK(chr)
